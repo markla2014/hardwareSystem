c16b5cc595f5b28256974fe9414dded6418cd3e1
markla <1398257720@qq.com>
2017-03-23 14:16:08 +0800

    init commit

---
+++ src/main/java/com/rofs/HardwaresystemApplication.java
@@ -0,0 +1,12 @@
+package com.rofs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HardwaresystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HardwaresystemApplication.class, args);
+	}
+}
